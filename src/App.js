990f3b29b6a87bcf6434ef71e22692d1f169da42
roneet002 <roneet.kumar002@gmail.com>
2020-03-16 23:35:18 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Home />
-      </header>
-    </div>
-  );
+export default class App extends Component{
+constructor(){
+super()
+this.state={
+user:null,
+password:null
+}
+}
+ sbt=()=>{
+  user = this.state.user;
+  password =this.state.password
+this.setState({
+
+  
+})
+
+}
+users=(event)=>{
+this.setState({
+  user:event.target.value
+})
+}
+password=(event)=>{
+  this.setState({
+    password:event.target.value
+  })
 }
 
-export default App;
+  render(){
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+
+          <Home />
+          
+          <input type="text" onChange={(event)=>{this.users(event)}} />
+          <input type="password"  onChange={(event)=>{this.password(event)}} />          
+          <button onClick={()=>{this.sbt()}}>Form submit</button>
+
+        {this.state.user}
+        {this.state.password}
+        </header>
+      </div>
+    );
+
+
+  }
+}
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home Component</h1>
-            </div>
-        )
-    }
+
+ function Home(){
+    const [count,setCount] = useState(100)
+
+    useEffect(() => {
+        console.warn(count)
+    }, [count])
+
+    return (
+        <div>
+            <h1>Home Component {count}</h1>
+            <button onClick={()=>{setCount(count-1)}}>count increase</button>
+        </div>
+    )
 }
+
+
+export default Home
